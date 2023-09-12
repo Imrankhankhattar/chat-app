3fe648a5c1575aee0502bfc56f9e9134e2bf2799
imran_khan_941 <imran.khan@tkxel.io>
2023-09-12 13:22:09 +0500

    server file changes

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ const app = express();
 const dotenv = require('dotenv')
 const webAPI = require("./WebAPI/user")
 const connectDB = require('./bin/db')
-const redisClient = require('./Services/redis')
+require('./Services/redis')
 dotenv.config();
+
 const PORT = process.env.PORT || 3000;
 connectDB()
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req,res,next)=>{
